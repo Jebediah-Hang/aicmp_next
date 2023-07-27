48c5b058f49a47cfafe1091259594e0cd3997180
Jebediah-Hang <zhrunhang@163.com>
2023-07-27 23:16:50 +0800

    Init Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <div class="text">SCSS TEST</div>
+    <el-button size="small">AAA</el-button>
+    <el-icon><Odometer /></el-icon>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.about {
+  .text {
+    color: blueviolet;
+  }
+}
+</style>
