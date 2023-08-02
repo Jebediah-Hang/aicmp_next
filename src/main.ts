c951c015d36bcaa01d615d0ce447a6be7531538f
Jebediah-Hang <zhrunhang@163.com>
2023-08-02 20:02:54 +0800

    remove element-icon, add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -21,9 +20,5 @@ const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 
 app.mount('#app')
--- src/views/home/desktop/index.vue
+++ src/views/home/desktop/index.vue
@@ -1,3 +1,4 @@
 <template>
   <div>Desktop Home</div>
+  <i class="fa fa-plane"></i>
 </template>
