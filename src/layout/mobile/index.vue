05aa6bdec0722d31119e8bfac2de7f81e419dca3
Jebediah-Hang <zhrunhang@163.com>
2023-08-02 21:13:03 +0800

    add mobile frame menu

--- src/layout/mobile/index.vue
+++ src/layout/mobile/index.vue
@@ -1,31 +1,58 @@
 <template>
-  <div class="content-main">
+  <div class="content-main" @scroll="contentScroll">
     <RouterView />
   </div>
   <div class="nav-bar">
-    <div class="nav-item"></div>
+    <div class="nav-item">
+      <i class="fa fa-home"></i>
+    </div>
+    <div class="nav-item">
+      <i class="fa fa-rocket"></i>
+    </div>
+    <div class="nav-item">
+      <i class="fa fa-compass"></i>
+    </div>
+    <div class="nav-item">
+      <i class="fa fa-cubes"></i>
+    </div>
+    <div class="nav-item">
+      <i class="fa fa-file-text-o"></i>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+function contentScroll(e: any) {
+  console.log(e)
+}
 </script>
 
 <style scoped lang="scss">
 .content-main {
-  padding-bottom: 30px;
+  padding-bottom: 50px;
 }
 .nav-bar {
   width: 100%;
-  height: 30px;
-  background-color: bisque;
+  height: 40px;
+  background-color: #fff;
+  border-top: rgba(0, 0, 0, 0.2) solid 1px;
   position: fixed;
   bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   .nav-item {
-    width: 100px;
-    height: 20px;
-    border: #000 solid 1px;
-    background-color: antiquewhite;
+    width: calc((100% - 40px) / 5);
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    > i {
+      color: #888;
+      font-size: 26px;
+    }
   }
 }
 </style>
--- src/views/home/mobile/index.vue
+++ src/views/home/mobile/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>Mobile Home</div>
+  <div class="main-content" @scroll="mScroll">
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nostrum eveniet maxime praesentium iste aliquid repudiandae perferendis ut molestiae cumque dolorem perspiciatis tempora, quia, quam eaque voluptate dolor quis quos.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum animi numquam consequuntur aspernatur quisquam suscipit sed nobis, vel cum earum, in atque, at nesciunt a repellat eveniet reiciendis distinctio delectus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, magni eum? Totam placeat dolores aut, eos sed, recusandae, animi nostrum soluta assumenda veritatis voluptatum dicta quos excepturi modi. Hic, accusamus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores error accusamus harum eos debitis saepe? Quae nemo commodi molestias dolores beatae ducimus, tempora, nobis itaque nisi sed veritatis voluptate tempore.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi accusamus quisquam odio, culpa expedita praesentium iste maiores explicabo nulla harum voluptates ullam libero eius laudantium, doloribus illo porro voluptatem.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, distinctio doloribus quisquam modi, eveniet, est numquam quae nihil dolorem voluptate debitis velit! Incidunt suscipit, maiores consequuntur porro accusantium excepturi ad?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum enim nihil odio quidem magni dicta debitis illo accusantium qui rerum quos facere dignissimos reiciendis nobis similique explicabo deleniti, cum esse?
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda omnis velit, nobis molestiae placeat voluptatem blanditiis architecto officia! Nobis quisquam minima neque aperiam necessitatibus nesciunt doloribus nihil aut assumenda sed?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem odit aliquam voluptatem dolores. Magnam officiis distinctio aliquid repudiandae rem aperiam dolorem. Consequuntur dolorem reprehenderit quia fugit explicabo officia commodi.
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+function mScroll(e: any) {
+  console.log('scroll', e)
+}
+</script>
+
+<style lang="scss" scoped>
+.main-content {
+  height: 2000px;
+}
+</style>
