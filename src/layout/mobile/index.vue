36c5a972aecb5676d36e194b77d1922fe6e3c18e
Jebediah-Hang <zhrunhang@163.com>
2023-08-02 23:04:32 +0800

    modify mobile menu style

--- src/layout/mobile/index.vue
+++ src/layout/mobile/index.vue
@@ -2,21 +2,26 @@
   <div class="content-main" @scroll="contentScroll">
     <RouterView />
   </div>
-  <div class="nav-bar">
+  <div class="nav-bar light">
     <div class="nav-item">
       <i class="fa fa-home"></i>
+      <div>HOME</div>
     </div>
     <div class="nav-item">
       <i class="fa fa-rocket"></i>
+      <div>MISSION</div>
     </div>
     <div class="nav-item">
       <i class="fa fa-compass"></i>
+      <div>TREND</div>
     </div>
     <div class="nav-item">
       <i class="fa fa-cubes"></i>
+      <div>RESOURCE</div>
     </div>
     <div class="nav-item">
-      <i class="fa fa-file-text-o"></i>
+      <i class="fa fa-book"></i>
+      <div>ARTICLE</div>
     </div>
   </div>
 </template>
@@ -31,28 +36,41 @@ function contentScroll(e: any) {
 
 <style scoped lang="scss">
 .content-main {
-  padding-bottom: 50px;
+  padding-bottom: 70px;
 }
 .nav-bar {
   width: 100%;
-  height: 40px;
-  background-color: #fff;
+  height: 60px;
   border-top: rgba(0, 0, 0, 0.2) solid 1px;
   position: fixed;
   bottom: 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  user-select: none;
   .nav-item {
     width: calc((100% - 40px) / 5);
-    height: 40px;
+    height: 100%;
     display: flex;
     align-items: center;
+    flex-direction: column;
     justify-content: center;
     > i {
-      color: #888;
-      font-size: 26px;
+      font-size: 20px;
+    }
+    > div {
+      font-size: 12px;
+      padding-top: 6px;
+      font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     }
   }
+}
+.light {
+  color: #111;
+  background-color: #fff;
+}
+.dark {
+  color: #fff;
+  background-color: #111;
 }
 </style>
--- src/views/home/mobile/index.vue
+++ src/views/home/mobile/index.vue
@@ -22,6 +22,6 @@ function mScroll(e: any) {
 
 <style lang="scss" scoped>
 .main-content {
-  height: 2000px;
+  // height: 2000px;
 }
 </style>
