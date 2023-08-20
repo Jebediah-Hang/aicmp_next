9e2c4289ab84349b9f576228c178335242634149
Jebediah-Hang <zhrunhang@163.com>
2023-08-20 15:04:27 +0800

    use cdn

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElDropdown: typeof import('element-plus/es')['ElDropdown']
-    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
-    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import importToCDN from 'vite-plugin-cdn-import'
 
 import { visualizer } from 'rollup-plugin-visualizer'
 
@@ -18,6 +19,30 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
+    }),
+    importToCDN({
+      modules: [
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: 'https://unpkg.com/vue@3.3.4/dist/vue.global.prod.js'
+        },
+        {
+          name: 'vue-i18n',
+          var: 'VueI18n',
+          path: 'https://unpkg.com/vue-i18n@9.3.0-beta.25/dist/vue-i18n.global.prod.js',
+        },
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: 'https://unpkg.com/vue-router@4.2.4/dist/vue-router.global.prod.js'
+        },
+        {
+          name: 'pinia',
+          var: 'Pinia',
+          path: 'https://unpkg.com/pinia@2.1.6/dist/pinia.iife.prod.js'
+        }
+      ]
     }),
     visualizer({
       open: true,
